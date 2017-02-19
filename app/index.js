9eda359a5ccd80c996b3b2da586fa16adf3d9a3c
Dongshuang.xds <dongshuang.xds@alibaba-inc.com>
2017-02-19 17:59:06 +0800

    s

--- app/index.js
+++ app/index.js
@@ -4,14 +4,43 @@ import Resource from './model'
 
 const p2p = new P2PSpider()
 
-p2p.filter((infohash, rinfo) => {
-    // false => always to download the metadata even though the metadata is exists.
-  const theInfohashIsExistsInDatabase = false
-  return theInfohashIsExistsInDatabase
+p2p.filter(async (infohash, rinfo) => {
+  const resource = await Resource.findOne({
+    where: {
+      infohash: infohash
+    }
+  })
+  return !!resource
 })
 
-p2p.on('metadata', (metadata) => {
-  console.log(metadata.info.name.toString('utf8'))
+/**
+ * infohash
+ * magnet
+ * address
+ * port
+ * info.name
+ * info.files
+ * info.piece length
+ * info.pieces
+ */
+p2p.on('metadata', async (metadata) => {
+  const [instance, created] = await Resource.findOrCreate({
+    where: {
+      infohash: metadata.infohash
+    },
+
+    defaults: {
+      magnet: metadata.magnet,
+      name: metadata.info.name.toString('utf8'),
+      score: 0
+    }
+  })
+
+  if (!created) {
+    instance.update({
+      score: instance.get('score') + 1
+    })
+  }
 })
 
 p2p.listen()
